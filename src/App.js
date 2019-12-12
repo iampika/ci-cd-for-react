71ed5e07d4332ea28b7e4cea95ddc38782296a37
Mahendra Choudhary <jakepintu@gmail.com>
2019-12-12 21:45:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="is-boxed has-animations">
+      <div className="body-wrap boxed-container">
+        <div className="container text-center mt-24">
+          <h1 className="text-secondary">App</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
