9ab12290c41c3e526dedb548dff394998f9c2010
Mahendra Choudhary <jakepintu@gmail.com>
2019-12-12 21:56:27 +0530

    Updates

---
+++ precache-manifest.92d5f63f890fbc5846e4ec5e7e5cff74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7ec97e9563d2f1eaaf0662c744d7d4",
+    "url": "/ci-cd-for-react/index.html"
+  },
+  {
+    "revision": "0a044b8e8cf0ee722138",
+    "url": "/ci-cd-for-react/static/css/main.aea3dbcb.chunk.css"
+  },
+  {
+    "revision": "4716f0948540aa8b0dd3",
+    "url": "/ci-cd-for-react/static/js/2.bd3173ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ci-cd-for-react/static/js/2.bd3173ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a044b8e8cf0ee722138",
+    "url": "/ci-cd-for-react/static/js/main.794cb47a.chunk.js"
+  },
+  {
+    "revision": "fa2c4383cbec5404e203",
+    "url": "/ci-cd-for-react/static/js/runtime-main.8fc00645.js"
+  }
+]);
