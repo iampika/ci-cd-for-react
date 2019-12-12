7afbfcbc2d66fb8350618f21b117c85a13347707
Mahendra Choudhary <jakepintu@gmail.com>
2019-12-12 22:34:50 +0530

    Update test heading

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/App/i);
+  const linkElement = getByText(/Continuous Integration/i);
   expect(linkElement).toBeInTheDocument();
 });
